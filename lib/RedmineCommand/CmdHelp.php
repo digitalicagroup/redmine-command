96583460f86d48f903a7517791a737d8e1e45337
Luis Augusto Peña Pereira <lpenap@gmail.com>
2014-12-10 17:42:35 -0430

    New commands definition facility.

--- lib/RedmineCommand/CmdHelp.php
+++ lib/RedmineCommand/CmdHelp.php
@@ -2,25 +2,39 @@
 
 namespace RedmineCommand;
 
+/**
+ * Class to parse help data (loaded by CommandFactory) into a SlackResult instance
+ * containing all available commands into fields of a single attachment.
+ *
+ * @author Luis Augusto Peña Pereira <lpenap at gmail dot com>
+ *        
+ */
 class CmdHelp extends AbstractCommand {
-	protected function executeImpl () {
+	protected function executeImpl() {
 		$log = $this->log;
 		$result = new SlackResult ();
-		$result->setText ("redmine-command help");
+		$result->setText ( "redmine-command help" );
 		$att = new SlackResultAttachment ();
-		$att->setTitle ("Available Commands:");
-		$att->setFallback ("Available Commands:");
-		$fields = array();
+		$att->setTitle ( "Available Commands:" );
+		$att->setFallback ( "Available Commands:" );
 		
-		$fields[] = SlackResultAttachmentField::withAttributes ("show", "<show> <issue numbers> Shows values for a list of issue numbers (space separated).", false);
-		/**
-		 * Add new instances of SlackResultAttachmentField here
-		 * representing help texts for each command.
-		 */
-		
-		usort ($fields, array ("RedmineCommand\SlackResultAttachmentField", "compare"));
-		$att->setFieldsArray ($fields);
-		$result->setAttachmentsArray (array($att));
+		$fields = array ();
+		$help_data = CommandFactory::getHelpData ();
+		if ($help_data == null) {
+			$log->error ( "CmdHelp: Error loading help data, check commands_definition.json format or file permissions" );
+		} else {
+			foreach ( $help_data as $h ) {
+				$fields [] = SlackResultAttachmentField::withAttributes ( $f ["help_title"], $f ["help_text"], false );
+			}
+		}
+		usort ( $fields, array (
+				"RedmineCommand\SlackResultAttachmentField",
+				"compare" 
+		) );
+		$att->setFieldsArray ( $fields );
+		$result->setAttachmentsArray ( array (
+				$att 
+		) );
 		return $result;
 	}
 }
--- lib/RedmineCommand/CommandFactory.php
+++ lib/RedmineCommand/CommandFactory.php
@@ -6,31 +6,42 @@ use Katzgrau\KLogger\Logger;
 
 /**
  * Class to handle input parameters parsing and command creation.
- * Aditional command classes only need to be added to attribute
- * $classes with a FQDN string.
+ * Aditional command classes must extend AbstractCommand and be
+ * added to commands_definition.json
  *
  * @author Luis Augusto Peña Pereira <lpenap at gmail dot com>
  *        
  */
 class CommandFactory {
+	protected static $classes = null;
+	protected static $help_data = null;
 	
 	/**
-	 * References to FQDN of command classes.
-	 * Aditional commands should
-	 * be added here only.
+	 * Factory method to create command instances.
+	 * New commands should be added to commands_definition.json
 	 *
-	 * @var array an array of strings representing FQDN of command classes.
+	 * @param array $post
+	 *        	Reference to $_POST
+	 * @param \RedmineCommand\Configuration $config
+	 *        	Configuration instance with parameters.
+	 * @return \RedmineCommand\AbstractCommand Returns an instance of an AbstractCommand subclass.
 	 */
-	protected static $classes = array (
-			"show" => "RedmineCommand\CmdShow",
-			"help" => "RedmineCommand\CmdHelp"
-	);
 	public static function create($post, $config) {
 		$cmd = new CmdUnknown ( $post, $config );
-		$log = new Logger ( $config->log_dir, $config->log_level );
 		$log->debug ( "CommandFactory: post received (json encoded): " . json_encode ( $post ) );
+		$log = new Logger ( $config->log_dir, $config->log_level );
+		
+		// checking if commands definitions have been loaded
+		if (self::$classes == null || self::$help_data == null) {
+			$result = self::reloadDefinitions ();
+			if ($result) {
+				$log->debug ( "CommandFactory: commands_definition.json loaded" );
+			} else {
+				$log->error ( "CommandFactory: Error loading commands_definition.json, check json format or file permissions." );
+			}
+		}
 		// TODO move strings parameter 'text' to global definition
-		if (isset ( $post ['text'] )) {
+		if (isset ( $post ['text'] ) && self::$classes != null) {
 			$log->debug ( "CommandFactory: text received: " . $post ['text'] );
 			// parsing inputs by space
 			$input = preg_split ( "/[\s]+/", $post ['text'] );
@@ -42,5 +53,32 @@ class CommandFactory {
 			}
 		}
 		return $cmd;
+	}
+	
+	/**
+	 * Read command definitions from commands_definition.json.
+	 *
+	 * @return boolean returns false if json could not be loaded, true otherwise.
+	 */
+	public static function reloadDefinitions() {
+		$result = false;
+		$json = json_decode ( file_get_contents ( "./commands_definition.json" ), true );
+		if ($json != null) {
+			self::$classes = array ();
+			self::$help_data = array ();
+			foreach ( $json ["commands"] as $command ) {
+				self::$classes [$command ["trigger"]] = $command ["class"];
+				self::$help_data [$command ["help_title"]] = $command ["help_text"];
+			}
+			$result = true;
+		}
+		return $result;
+	}
+	
+	public static function getHelpData() {
+		if (self::$help_data == null) {
+			self::reloadDefinitions();
+		}
+		return self::$help_data;
 	}
 }
